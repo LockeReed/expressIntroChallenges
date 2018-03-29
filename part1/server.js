2b19ce373fb5066bda2d98be57ed8ee9a70a88f3
Locke <lockereed@gmail.com>
2018-03-29 13:30:05 -0700

    challenge 1

--- part1/server.js
+++ part1/server.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/hello', function(req, res) {
+   res.send("Hello!");
 });
 
 app.use(function(req, res) {
-  res.sendStatus(404);
+   res.sendStatus(404);
 });
 
 app.listen(port, function() {
-  console.log('Listening on port', port);
+   console.log('Listening on port', port);
 });
