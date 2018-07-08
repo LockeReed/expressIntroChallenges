6af2e7f65283f92fdc779ac281b6c06c767940ad
Locke Reed <lockereed@gmail.com>
2018-07-08 00:42:01 -0700

    bye bye

--- part1/server.js
+++ part1/server.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var port = process.env.PORT || 8000;
 
+app.get('/', function(req, res) {
+   res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.get('/verify/:age', function(req, res) {
+   let age = parseInt(req.params.age);
+   if (age > 13) {
+      res.sendStatus(200);
+   } else {
+      res.sendStatus(403);
+   }
+
+
+})
+
 app.get('/hello', function(req, res) {
-   res.send("Hello!");
+   res.send("Hello");
 });
+
+app.post('/create/:name', function(req, res) {
+
+   res.json({
+      id: 1,
+      name: req.params.name
+   })
+})
 
 app.use(function(req, res) {
    res.sendStatus(404);
